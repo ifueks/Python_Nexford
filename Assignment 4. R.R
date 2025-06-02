473cebf9ee3420414a523bea219a9d3a3cad937a
ifueks <37170461+ifueks@users.noreply.github.com>
2025-06-01 20:58:27 -0600

    Add files via upload

---
+++ Assignment 4. R.R
@@ -0,0 +1,14 @@
+install.packages("ggplot2")
+library(ggplot2)
+genre_data <- read.csv("C:/Users/eliri/Downloads/genre_counts.csv", header=TRUE)
+# Rename columns for clarity
+colnames(genre_data) <- c("Genre", "Count")
+
+# Plot
+ggplot(genre_data, aes(x=reorder(Genre, Count), y=Count)) +
+  geom_col(fill="steelblue") +
+  coord_flip() +
+  labs(title="Top 10 Most Frequent Genres on Netflix",
+       x="Genre",
+       y="Number of Titles") +
+  theme_minimal()
